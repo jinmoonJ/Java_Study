0854a9fb14c02583db8c5c36d91e341195e6d1d4
jinmoon <jjm12612@gmail.com>
2023-11-11 01:54:01 +0900

    java

---
+++ chap_01/_03_Variables.java
@@ -0,0 +1,13 @@
+package chap_01;
+
+public class _03_Variables {
+    public static void main(String[] args) {
+        String name;
+        name = "JM";
+        // String name = "JM"; 같음
+
+        System.out.println(name + "님, 배송이 시작 됩니다. 15시에 방문 예정입니다.");
+        System.out.println(name + "님, 배송이 완료되었습니다.");
+
+    }
+}
