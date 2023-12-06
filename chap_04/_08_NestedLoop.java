95a60b2b7c8cd04359b883e3f1f845ba36c6ba96
jinmoon <jjm12612@gmail.com>
2023-12-07 00:29:21 +0900

    todo Study

---
+++ chap_04/_08_NestedLoop.java
@@ -0,0 +1,76 @@
+package chap_04;
+
+public class _08_NestedLoop {
+    public static void main(String[] args) {
+        // 이중 반복문
+
+        //  별 (*) 사각형 만들기
+        /*
+
+        *****
+        *****
+        *****
+        *****
+        *****
+
+        */
+
+        for (int i = 0; i < 5; i++) { // 몇 행인지
+            for (int j = 0; j < 5; j++) { // 한 줄 ***** 출력
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("====================");
+
+        // 별 (*) 왼쪽 삼각형 만들기
+        /*
+
+        *
+        **
+        ***
+        ****
+        *****
+
+         */
+        for (int i = 0; i < 5; i++) {
+            //for (int j = 0; j < i + 1; j++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("==============");
+
+        // 별 (*) 오른쪽 삼각형 만들기
+        /*
+
+             *
+            **
+           ***
+          ****
+         *****
+
+         SSSS*
+         SSS**
+         SS***
+         S****
+         *****
+
+         */
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 4 - i; j++) {
+                System.out.print("S");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+        }
+
+    }
+}
